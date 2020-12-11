fa30da56d609051e2796e13301eed81cd895c6cb
Preston Matheson <pmphoto123@gmail.com>
2020-12-10 19:09:02 -0800

    update about

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -14,7 +14,7 @@ function LandingPage(props) {
             About
           </h5>
           <div className='p--16'>
-            Hey there! I'm Preston:) A Software Engineer, video game enthusiest, and doggo dad currently based in Oregon! I got into programming with it's constant need for learning and as an outlet for creativity. Outside of the programming world I enjoy spending time with my dog, grabbing a drink with friends, playing video games, enjoying a good book, or spending time in nature. Feel free to explore my website or get in touch on one of my listed accounts.
+            Hey there! I'm Preston:) A Software Engineer, video game enthusiest, and doggo dad currently based in Oregon! I got into programming because of it's constant need for learning and as an outlet for creativity. Outside of the programming world I enjoy spending time with my dog, grabbing a drink with friends, playing video games, enjoying a good book, or spending time in nature. Feel free to explore my website or get in touch on one of my listed accounts.
           </div>
         </div>
         <div className='card m-vertical--16'>
